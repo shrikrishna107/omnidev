a713418f39eefada43074d68443bcdc3132b8968
shrikrishna107 <shrikrishnap107@gmail.com>
2024-06-16 13:05:35 +0530

    removed linting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+      ignore: ['path/to/file.js'],
+    },
+  };
+  
+  module.exports = nextConfig;
